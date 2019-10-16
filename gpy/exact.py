b50ebe18c9c66ebeac38cfe11e8b6106c5573923
Eman Johnson <emanjohnson91@gmail.com>
2019-10-16 17:56:53 +0200

    Added unscented predictive variance estimates.
    GPR and Sparse GPR implementation.
    Changed the names.
    Updates the demo notebook.
    Removed failing minibatching.

--- gpy/exact.py
+++ gpy/exact.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.metrics import r2_score
 
+
 class GPR(BaseEstimator, RegressorMixin):
     def __init__(
         self,
@@ -31,40 +32,40 @@ class GPR(BaseEstimator, RegressorMixin):
 
         # Kernel matrix
         gp_model = GPy.models.GPRegression(X, y, kernel)
-        
+
         # Optimization
         if self.n_restarts >= 1:
             gp_model.optimize_restarts(
                 num_restarts=self.n_restarts,
-                robust=True, 
+                robust=True,
                 verbose=self.verbose,
-                max_iters=self.max_iters
+                max_iters=self.max_iters,
             )
         else:
             gp_model.optimize(
-                self.optimizer, 
-                messages=self.verbose, 
-                max_iters=self.max_iters
+                self.optimizer, messages=self.verbose, max_iters=self.max_iters
             )
-        
 
         self.gp_model = gp_model
 
         return self
 
-
     def display_model(self):
         return self.gp_model
 
-    def predict(self, X, return_std=False, noiseless=True):
-        
-        if noiseless:
-            mean, var = self.gp_model.predict_noiseless(X)
-        else: 
-            mean, var = self.gp_model.predict(X)
+    def predict(self, X: np.ndarray, return_std: bool = False, noiseless: bool = True):
+
+        if noiseless == True:
+            include_likelihood = False
+        elif noiseless == False:
+            include_likelihood = True
+        else:
+            raise ValueError(f"Unrecognized argument for noiseless: {noiseless}")
+
+        mean, var = self.gp_model.predict(X, include_likelihood=include_likelihood)
+
         if return_std:
             return mean, np.sqrt(var)
         else:
             return mean
-
 
